9af588e79fbe8471664a8ea2026ba67ce2b13582
awesome.however <awesome.however@yandex.ru>
2023-04-18 22:41:40 +0300

    Apply custom template

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
-import React from 'react';
-import ToDoTaskAdd from './ToDoTaskAdd';
-import ToDoList from './ToDoList';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import { todoAddAll } from './actions'
-import { Provider, connect } from 'react-redux';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends React.Component {
-  
-  componentDidMount() {
-    fetch('tasks').then(function(res) {
-      return res.json();
-    }).then((data) => { 
-		 this.props.dispatch(todoAddAll(data));
-    });
-  }
-
-  render(){
-    return (
-      <div className="row d-flex justify-content-center container">
-    <div className="col-md-8">
-			  <Provider store={this.props.store}>
-				<Router>
-				  <Routes>
-					<Route path="/" element={<ToDoList />} />
-					<Route path="/add" element={<ToDoTaskAdd />} />
-				  </Routes>
-				</Router>
-			  </Provider>
-		</div>
-	  </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default connect()(App);
+export default App;
